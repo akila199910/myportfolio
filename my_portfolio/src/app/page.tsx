7743be313c14a37560a8b40f767953d1828e695b
Akila-PHP <akila-php@webappclouds.com>
2024-09-12 17:40:46 +0530

    create hero and profile sections

--- my_portfolio/src/app/page.tsx
+++ my_portfolio/src/app/page.tsx
@@ -1,4 +1,9 @@
 import { FaMoon } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import Image from "next/image";
+import profileImage from "./public/p2.png";
+
 export default function Home() {
   return (
     <main className=" bg-yellow-50 min-h-screen px-10">
@@ -20,6 +25,30 @@ export default function Home() {
             </li>
           </ul>
         </nav>
+        {/* {this is hero part} */}
+        <div>
+          <h1>Akila Umayanga</h1>
+          <h3>Software Developer</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure fuga
+            dolor dicta quos, optio sequi facilis sint explicabo neque
+            architecto maxime incidunt ducimus quidem, mollitia tenetur
+            distinctio. Repellat, quidem itaque.
+          </p>
+        </div>
+        {/* sosial */}
+        <div>
+          <a href="">
+            <FaGithub />
+          </a>
+          <a href="">
+            <CiLinkedin />
+          </a>
+        </div>
+        {/* profile */}
+        <div>
+          <Image src={profileImage} alt="profile image" />
+        </div>
       </section>
     </main>
   );
