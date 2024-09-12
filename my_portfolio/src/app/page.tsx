3b5b6d7ad400b1f626ae1c0241d3b6a8671d7f64
Akila-PHP <akila-php@webappclouds.com>
2024-09-12 17:16:33 +0530

    add styles for nav section mobile view

--- my_portfolio/src/app/page.tsx
+++ my_portfolio/src/app/page.tsx
@@ -1,16 +1,22 @@
 import { FaMoon } from "react-icons/fa";
 export default function Home() {
   return (
-    <main>
+    <main className=" bg-yellow-50 min-h-screen px-10">
       <section>
-        <nav>
-          <h1>Develop b atomic arts</h1>
-          <ul>
+        <nav className=" py-10 flex justify-between">
+          <h1 className=" text-sm">Develop b atomic arts</h1>
+          <ul className=" flex items-center">
             <li>
-              <FaMoon />
+              <FaMoon className=" cursor-pointer text-xl mx-5" />
             </li>
             <li>
-              <a href="">Resume</a>
+              <a
+                href=""
+                className="bg-yellow-400 text-black 
+                px-4 py-2 border-none rounded-md"
+              >
+                Resume
+              </a>
             </li>
           </ul>
         </nav>
